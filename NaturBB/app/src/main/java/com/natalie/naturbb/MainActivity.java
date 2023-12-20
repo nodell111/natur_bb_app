4fdd016eb72c5769003ae914a5d21153a7b552ad
dbozkurt <dlara.bozkurt@gmail.com>
2023-12-20 10:37:03 +0100

    info page_content lorem The MainActivity java is empty except the functions for opening the info page

--- NaturBB/app/src/main/java/com/natalie/naturbb/MainActivity.java
+++ NaturBB/app/src/main/java/com/natalie/naturbb/MainActivity.java
@@ -2,13 +2,30 @@ package com.natalie.naturbb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton info_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the ImageButton by its ID
+        info_Button = findViewById(R.id.infoButton);
+
+        info_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
